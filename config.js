dec5f860184f9af14c9561e55e4ce1419fd790bb
hb <hb@appledeMacBook-Air.local>
2026-08-02 15:44:37 +0800

    添加频道

--- config.js
+++ config.js
@@ -5,6 +5,8 @@
 const CHANNELS = [
   { id: "UCBKDRq35-L8xev4O7ZqBeLg", name: "王志安" },
   { id: "UCIXOIjR2mp8tHz78DE0vj2A", name: "徐晓冬" },
+  { id: "UCfYYi6aJNg70mFYnbBKcMQw", name: "A.J.Hoge" },
+  { id: "UCnWhDyhWyOzplLXo50BHMpg", name: "Pullman Gym - 拳击" },
   // 添加更多频道就复制一行，改成对应的频道 ID 和名字，例如：
   // { id: "UCxxxxxxxxxxxxxxxxxxxxxx", name: "频道名" },
 ];
